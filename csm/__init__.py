895ee75710fa7ecf40bd0a6bd7e4f9580d19e3e9
TibetanSandFox <pawel.zyblewski@pwr.edu.pl>
2019-03-09 17:23:51 +0100

    init

--- csm/__init__.py
+++ csm/__init__.py
@@ -2,5 +2,6 @@ from .StreamGenerator import StreamGenerator
 from .Dumb import Dumb
 from .TestAndTrain import TestAndTrain
 from .DESlibStream import DESlibStream
+from .DESlibKEEL import DESlibKEEL
 
-__all__ = ["StreamGenerator", "Dumb", "TestAndTrain", "DESlibStream"]
+__all__ = ["StreamGenerator", "Dumb", "TestAndTrain", "DESlibStream", "DESlibKEEL"]

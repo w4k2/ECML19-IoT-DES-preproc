6233a8e2ea6bf86230d9fc8ec0fb5a2bd059adaa
TibetanSandFox <pawel.zyblewski@pwr.edu.pl>
2019-03-10 15:37:21 +0100

    basic keel run

--- rbo_test.py
+++ rbo_test.py
@@ -1,10 +1,5 @@
 from RBO import RBO
-from imblearn.over_sampling import RandomOverSampler, SMOTE, SVMSMOTE, BorderlineSMOTE, ADASYN
-
 import helper as h
-
-smote = SMOTE(random_state=42)
-rand = RandomOverSampler(random_state=42)
 
 X, y = h.datImport("ecoli4.dat")
 
@@ -12,8 +7,7 @@ print(X.shape)
 print(y.shape)
 
 rbo = RBO()
-# X, y = rbo.fit_sample(X, y)
-X, y = smote.fit_resample(X, y)
+X, y = rbo.fit_sample(X, y)
 
 print(X.shape)
 print(y.shape)
